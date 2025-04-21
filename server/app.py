55623ec2b5b31290ef95795f8dd4e4a7653aef2c
Shakira Syevuo <shakirasyevuo@gmail.com>
2025-04-21 13:35:31 +0300

    Completed lab

--- server/app.py
+++ server/app.py
@@ -28,16 +28,44 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    # check if the user is logged in
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                return user.to_dict(), 200
+        return {}, 204
+
+
 
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        username = json.get('username')
+        password = json.get('password')
+
+        user = User.query.filter_by(username=username).first()
+
+        # checks the password
+        if user and user.authenticate(password):
+            #  if authenticated returns users data
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        
+        return {'error': 'Invalid username or password'}, 401
 
 class Logout(Resource):
-    pass
+    # clear the session
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
